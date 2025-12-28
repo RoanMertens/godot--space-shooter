8aed11a7abac336878e598001b337f8592e66263
R.Mertens <contact@rmertens.com>
2025-12-28 12:52:30 +0000

    fix no lives displaying before getting hit

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -2,8 +2,18 @@ extends CanvasLayer
 
 var image = load("res://.godot/imported/playerLife3_red.png-9ad8590d8b36726ff67a57a103fa6762.ctex")
 
+func _ready() -> void:
+	refresh_player_lives()
+
 func _on_player_takes_damage() -> void:
-	# remove old displayed of lives
+	refresh_player_lives()
+
+func _on_level_score_updated() -> void:
+	# update displayed score
+	$Score/ScoreLabel.text = 'Score: ' + str(Global.score)
+
+func refresh_player_lives() -> void:
+		# remove old displayed of lives
 	for child in $Health/Lives.get_children():
 		child.queue_free()
 
@@ -13,7 +23,3 @@ func _on_player_takes_damage() -> void:
 		text_rectangle.texture = image
 		text_rectangle.stretch_mode = TextureRect.STRETCH_KEEP
 		$Health/Lives.add_child(text_rectangle)
-
-func _on_level_score_updated() -> void:
-	# update displayed score
-	$Score/ScoreLabel.text = 'Score: ' + str(Global.score)
